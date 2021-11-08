c2427af67e5ace9ce73abb7d6fb8a603ffd2a2c0
Temur <timurzaitov1998@gmail.com>
2021-11-08 18:15:51 +0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Temur on 08/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
